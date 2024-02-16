41fef55b178400c9797b42817e7bd734de757892
Deep <debojeetkarmakar2020@gmail.com>
2024-02-16 17:52:16 +0530

    adding qr generator

---
+++ qr generator/script.js
@@ -0,0 +1,28 @@
+const textinput = document.querySelector('#textInput');
+const btn = document.querySelector('#create');
+const imgbox = document.querySelector('.imgbox');
+const del = document.querySelector('#delete');
+console.log(del);
+
+
+
+
+
+
+
+
+function generateQRCode () {
+    let text = textinput.value; 
+    let qr = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+    imgbox.innerHTML = `<img src="${qr}" id="qrimg" alt="">`;
+}
+
+
+
+btn.addEventListener("click", function(e) {
+    generateQRCode()
+});
+
+del.addEventListener("click", function(e) {
+    imgbox.innerHTML = ``;
+});
