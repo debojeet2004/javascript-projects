ed322fa6215c222c4abebb2a52035c39f43b0cfe
Deep <debojeetkarmakar2020@gmail.com>
2024-02-17 22:04:36 +0530

    some small changes

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,7 +1,10 @@
 let hrs = document.getElementById('hrs');
 let sec = document.getElementById('sec');
 let min = document.getElementById('min');
-
+console.log(new Date())
+console.log(new Date().getMonth())
+console.log(new Date().getDay())
+// console.log(new Date().getDate())
 setInterval(() => {
     let time = new Date();
     hrs.innerHTML = (time.getHours()<10? "0" : "") + time.getHours();
