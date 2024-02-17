fbc80bdb277efe337f92a0463e766aefe4f752ba
Deep <debojeetkarmakar2020@gmail.com>
2024-02-17 15:15:55 +0530

    digital clock

---
+++ digital-clock/script.js
@@ -0,0 +1,11 @@
+let hrs = document.getElementById('hrs');
+let sec = document.getElementById('sec');
+let min = document.getElementById('min');
+
+setInterval(() => {
+    let time = new Date();
+    hrs.innerHTML = (time.getHours()<10? "0" : "") + time.getHours();
+    min.innerHTML = (time.getMinutes()<10? "0" : "") + time.getMinutes();
+    sec.innerHTML = (time.getSeconds()<10? "0" : "") + time.getSeconds();
+},1000);
+
