83901029b8ec7dd3c25812bb5627377b0b1d29d5
Deep <debojeetkarmakar2020@gmail.com>
2024-02-21 18:26:48 +0530

    folder name changed

--- qr generator✅/script.js
+++ qr generator✅/script.js
@@ -2,27 +2,19 @@ const textinput = document.querySelector('#textInput');
 const btn = document.querySelector('#create');
 const imgbox = document.querySelector('.imgbox');
 const del = document.querySelector('#delete');
-console.log(del);
-
-
-
-
-
-
-
+// console.log(del);
 
 function generateQRCode () {
     let text = textinput.value; 
     let qr = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
     imgbox.innerHTML = `<img src="${qr}" id="qrimg" alt="">`;
 }
-
-
-
 btn.addEventListener("click", function(e) {
     generateQRCode()
+    
 });
 
 del.addEventListener("click", function(e) {
     imgbox.innerHTML = ``;
+    textinput.value= " ";
 });
